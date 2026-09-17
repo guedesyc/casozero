f6404684616f04d2675bc531be89c4d23218afdd
Yago Guedes <115386571+guedesyc@users.noreply.github.com>
2026-09-16 22:08:39 -0300

    fix: publish landing page at repository root

---
+++ _next/static/chunks/app/admin/advogados/[id]/page-83793cbd6562774c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[53],{},_=>{_.O(0,[619,16,441,255,358],()=>_(_.s=7016)),_N_E=_.O()}]);
---
+++ _next/static/chunks/app/app/casos/page-8010249c7497c945.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[418],{},_=>{_.O(0,[619,250,36,441,255,358],()=>_(_.s=36)),_N_E=_.O()}]);
